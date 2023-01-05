a2b26ef6a2924e265d4dcc3a08e5842522657071
Francisco Lopez <jflopezfranco@gmail.com>
2023-01-05 13:09:51 -0600

    Gana 3 veces usando WHILE

--- algoritmo-ppt/ppt.js
+++ algoritmo-ppt/ppt.js
@@ -16,7 +16,12 @@ function eleccion(jugada){
 }
 // 1 es piedra, 2 es papel y 3 es tijera
 let jugador = 0
-let pc = aleatorio(1,3)
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+
+while(triunfos < 3 && perdidas <3){
+    pc = aleatorio(1,3)
 
 jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
 // alert("Elegiste " + jugador)
@@ -29,11 +34,18 @@ alert("PC elige: " + eleccion(pc))
 if (jugador == pc){
         alert("EMPATE")
 }   else if (jugador == 1 && pc == 3){//piedra y tijeras
-        alert("Ganaste")
+        alert("Ganaste") 
+        triunfos = triunfos + 1
 }   else if (jugador == 2 && pc == 1){//papel y piedra
         alert("Ganaste")
+        triunfos = triunfos + 1
 }   else if (jugador == 3 && pc == 2){//tijeras y papel
         alert("Ganaste")
+        triunfos = triunfos + 1
 }   else {
         alert("Perdiste")
+        perdidas = perdidas + 1
 }
+}
+
+alert ("Ganaster " + triunfos + " veces y Perdiste "+ perdidas) + " veces."
