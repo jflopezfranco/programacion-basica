b0758c9840fa12834aaa169eca83c13895d832e2
Francisco Lopez <jflopezfranco@gmail.com>
2023-01-09 13:14:41 -0600

    Manipulación del DOM

--- mokepon/java-script/mokepon.js
+++ mokepon/java-script/mokepon.js
@@ -1,18 +1,20 @@
 function iniciar_juego(){
     let boton_mascota_jugador = document.getElementById("selector-mascota")
-    boton_mascota_jugador.addEventListener("click", selector_mascota_jugador)
+    boton_mascota_jugador.addEventListener("click", selector_mascota_jugador)  
 }
 
 function selector_mascota_jugador(){
 let input_hipodogue = document.getElementById("hipodogue")
 let input_capipepo = document.getElementById("capipepo")
 let input_ratigueya = document.getElementById("ratigueya")
+let span_mascota_jugador = document.getElementById("mascota-jugador")
+
     if (input_hipodogue.checked){
-        alert("Seleccionaste a Hipodogue")
+        span_mascota_jugador.innerHTML = "Hipodogue"
     }   else if (input_capipepo.checked){
-        alert("Seleccionaste a Capipepo")
+        span_mascota_jugador.innerHTML = "Capipepo"
     }   else if(input_ratigueya.checked){
-        alert("Seleccionaste a Ratigueya")
+        span_mascota_jugador.innerHTML = "Ratigueya"
     } else {
         alert("input incorreto. Selecciona una mascota para iniciar")
     }
