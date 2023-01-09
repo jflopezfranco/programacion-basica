efa84127c4f7913aa762027938e3cf54576fdd53
Francisco Lopez <jflopezfranco@gmail.com>
2023-01-09 12:53:21 -0600

    Escuchando Eventos con JavaScript

--- mokepon/java-script/mokepon.js
+++ mokepon/java-script/mokepon.js
@@ -1,6 +1,10 @@
-let boton_mascota_jugador = document.getElementById("selector-mascota")
-boton_mascota_jugador.addEventListener("click", selector_mascota_jugador)
+function iniciar_juego(){
+    let boton_mascota_jugador = document.getElementById("selector-mascota")
+    boton_mascota_jugador.addEventListener("click", selector_mascota_jugador)
+}
 
 function selector_mascota_jugador(){
     alert("seleccionaste tu mascota")
 }
+
+window.addEventListener("load", iniciar_juego)
